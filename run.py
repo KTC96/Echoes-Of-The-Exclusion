41c7797a4577bfc2de8782a7902d457134bd3468
KTC96 <kyleclow96@gmail.com>
2023-07-16 18:51:59 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+while True:
+    def game_introduction():
+        """
+        Function to introduce the game and ask the user if they would like to play.
+        """
+        play_decision = input("Would you like to play? (yes/no)")
+        if play_decision.lower().strip() == "yes":
+            start_zone()
+        else:
+            print("Understood, you are not ready for the challenge...")
+
+        
+    def start_zone():
+        print("hello")
+        
+    def main_game():
+        game_introduction()
+
+    
